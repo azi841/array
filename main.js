6d82a0d855fec52dd6413ac86196eb2cd27812c3
amehic <aazzhhaarroo@gmail.com>
2022-09-01 20:56:06 +0200

    asdasd

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+
+var array = [];
+var size = 5;
+
+for (var a=0; a<size; a++){
+    array[a] = prompt('Enter element' + (a+1));
+}
+
+
+numberOfArrays = 0;
+
+
+
+
+numberOfArrays = prompt();
+array2= [];
+
+ for (a=0; a<numberOfArrays; a++){
+    array2[a] = array[a]; 
+}
+
+alert(array2);
+
+
+
+
+
+
+
+//console.log(array);
+
